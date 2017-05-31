52581462373d6e5003de5146a720b755cd5df6c4
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-05-31 15:15:15 -0400

    fixed sprinkles

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,9 @@ console.log("hey?")
 function makeSprinkles(){
 	var x= document.getElementById("sprinkles")
 
-	if (x.style.display==="none"){
+	if (x.style.display !=="block"){
 		x.style.display= 'block';
 	} else {
 		x.style.display='none';
 	}
 }
-
